fafd38f7aa5c852f853acc7563c82608f5b4e9b2
Yashwant <yashrathore5598@gmail.com>
2023-09-12 11:05:58 +0530

    Pharmancy Management  Updated

--- src/main/java/com/oneHealth/PharmacyManagementServiceApplication.java
+++ src/main/java/com/oneHealth/PharmacyManagementServiceApplication.java
@@ -2,6 +2,7 @@ package com.oneHealth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class PharmacyManagementServiceApplication {
@@ -9,5 +10,13 @@ public class PharmacyManagementServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PharmacyManagementServiceApplication.class, args);
 	}
+	@GetMapping
+    public String Welcome() {
+
+        // This method handles GET requests to the root URL and returns a welcome message.
+
+        return "Welcome From OneHealth Team (Pharmacy Management Service)!!!";
+
+    }
 
 }
